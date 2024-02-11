de25a7d3539375d3c740b6223215b04b6421a0c4
H x T <157820850+hackingxtutorial-HxT@users.noreply.github.com>
2024-02-11 10:49:17 +0530

    Add files via upload

---
+++ HxT-IP_FINDER.py
@@ -0,0 +1,27 @@
+print ("\033[91m")
+import socket
+
+def get_ip_address(url):
+    ip_address = socket.gethostbyname(url)
+    return ip_address
+
+def main():
+    while True:
+        url = input("ENTER THE URL: ")
+
+        ip_address = get_ip_address(url)
+        print(" ")
+        print ("\033[96mCODED BY : hacking_x_tutorial\033[0m")
+        print(" ")
+        print("\033[96mSOCIAL MEDIA : https://sites.google.com/view/hachingxtutorial/home")
+        print(" ")
+        print("\033[91mDISCLAIMER - THIS TOOL IS ILLEGAL & IT'S ONLY FOR EDUCATION PURPOSE !  USE  IT AT YOUR OWN RISK , I AM NOT RESPONSIBLE FOR YOUR ACTION !\033[0m")
+        print(" ")
+        print("\033[91mIP Address:", ip_address)
+        print(" ")
+        next_url = input("DO YOU WANT ENTER ANOTHER URL? (y/n): ")
+        if next_url.lower() != "y":
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,27 @@
+# HxT-IP_FINDER
+
+An IP finder is a tool that can help you find out the location and other information of an IP address. An IP address is a unique identifier that is assigned to every device that connects to the internet. By using an IP finder, you can check the country, city, region, ISP, organization, and other details of an IP address.
+
+This tool is used to find the ip address of an website..
+
+$ apt update && apt upgrade
+
+$ pkg install python
+
+$ pkg install git
+
+$ pkg install figlet
+
+$ git clone https://github.com/hackingxtutorial-HxT/HxT-IP_FINDER.git
+
+$ cd HxT-IP_FINDER
+
+$ chmod +x HxT-IP_FINDER.py
+
+$ python HxT-IP_FINDER.py
+
+TO RUN :
+
+$ chmod +x HxT-IP_FINDER.py
+
+$ python HxT-IP_FINDER.py
